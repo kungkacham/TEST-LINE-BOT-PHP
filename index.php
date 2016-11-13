f5b2c7a8f444fb4b5b89d389048ce725c949536a
kungkacham <kungkacham.astrology@gmail.com>
2016-11-13 22:00:02 +0700

    index

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ if (!is_null($events['events'])) {
 				'text' => กาก
 			];}
 			
-			else{$messages = [
-				'type' => 'text',
-				'text' => $text}
+			//else{$messages = [
+			//	'type' => 'text',
+			//	'text' => $text}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
