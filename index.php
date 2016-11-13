874e7b39e236737e1d6e9ce63820ecd272ba4e8b
kungkacham <kungkacham.astrology@gmail.com>
2016-11-13 19:50:15 +0700

    index

--- index.php
+++ index.php
@@ -17,22 +17,18 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text == 'ก้อง'){ 
 			$messages = [
-				if($text == 'ก้อง')
-				'type' => 'text',
-				'text' => กาก;
-				else 'type' => 'text',
-				'text' => $text;
-			//	'text' => $text
-			];
-					
+			'type' => 'text',	
+			'text' => กาก
+			]
+}
+			else{
+			$messages = [
+			'type' => 'text',	
+			'text' => $text
+}
 				
-			
-			
-			
-			
-			
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
