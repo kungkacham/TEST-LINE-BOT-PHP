42c084019beaf27678aad49925891e41f73d2060
kungkacham <kungkacham.astrology@gmail.com>
2016-11-13 21:08:18 +0700

    index

--- index.php
+++ index.php
@@ -4,44 +4,13 @@ $access_token = 'QkERSr95E8xPvTP40mlaIZsY7hBwDKusWmPcp3jChPSxaUF/KNcs8RaVJiThluW
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+$jsonObj = json_decode($json_string);
+$to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
+$text = $jsonObj->{"result"}[0]->{"content"}->{"text"};
+$text_ex = explode(':', $text);
+$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>$result_text];
+if($text_ex[0] == "ก้อง"){
+$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"กาก"];
+}
 }
 echo "OK";
