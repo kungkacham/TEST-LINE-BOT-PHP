88956b60b24f011a518964588bf028685e24446f
kungkacham <kungkacham.astrology@gmail.com>
2016-11-13 20:07:49 +0700

    index

--- index.php
+++ index.php
@@ -22,11 +22,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => กาก
 			];}
-			else{$messages = [
-				'type' => 'text',
-				'text' => $text
-			];}
-
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
