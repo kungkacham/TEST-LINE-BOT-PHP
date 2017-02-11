f33fd54e872e0bed2b128911d665c235522542fb
kungkacham <kungkacham.astrology@gmail.com>
2017-02-12 03:01:56 +0700

    edit

--- index.php
+++ index.php
@@ -35,35 +35,7 @@ if (!is_null($events['events'])) {
 				'text' => setlastpoint
 			];}
 //			
-namespace LINE\Tests\LINEBot;
-use LINE\LINEBot;
-use LINE\Tests\LINEBot\Util\DummyHttpClient;
-class GetProfileTest extends \PHPUnit_Framework_TestCase
-{
-    public function testGetProfile()
-    {
-        $mock = function ($testRunner, $httpMethod, $url, $data) {
-            /** @var \PHPUnit_Framework_TestCase $testRunner */
-            $testRunner->assertEquals('GET', $httpMethod);
-            $testRunner->assertEquals('https://api.line.me/v2/bot/profile/USER_ID', $url);
-            return [
-                'displayName' => 'BOT API',
-                'userId' => 'userId',
-                'pictureUrl' => 'https://example.com/abcdefghijklmn',
-                'statusMessage' => 'Hello, LINE!',
-            ];
-        };
-        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'CHANNEL-SECRET']);
-        $res = $bot->getProfile('USER_ID');
-        $this->assertEquals(200, $res->getHTTPStatus());
-        $this->assertTrue($res->isSucceeded());
-        $data = $res->getJSONDecodedBody();
-        $this->assertEquals('BOT API', $data['displayName']);
-        $this->assertEquals('userId', $data['userId']);
-        $this->assertEquals('https://example.com/abcdefghijklmn', $data['pictureUrl']);
-        $this->assertEquals('Hello, LINE!', $data['statusMessage']);
-    }
-}
+
 //			
 			//else{$messages = [
 			//	'type' => 'text',
