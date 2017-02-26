54ae25085be2776eb881989ccc36bfbfe690c87e
kungkacham <kungkacham.astrology@gmail.com>
2017-02-27 06:36:33 +0700

    edit

--- index.php
+++ index.php
@@ -175,17 +175,17 @@ if (!is_null($events['events'])) {
 			//‘stickerId’ => ‘300’
 			//);
 		
-			$sticker = array(
+			$sticker = [
 			‘type’ => ‘sticker’,
 			‘packageId’ => ‘4’,
 			‘stickerId’ => ‘300’
-			);
+			];
  
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$packet = array(
 			‘replyToken’ => $replyToken,
-			‘messages’ => array($sticker),
+			‘messages’ => [$sticker],
 			);
 			$post = json_encode($packet);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
