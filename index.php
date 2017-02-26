745abcfdc9cf3e760dd3397b371b97b631c70e4f
kungkacham <kungkacham.astrology@gmail.com>
2017-02-27 05:19:51 +0700

    edit

--- index.php
+++ index.php
@@ -133,9 +133,9 @@ if (!is_null($events['events'])) {
 			//	'STKVER' => '100'
 			//];}
 			
-			//else{$messages = [
-			//	'type' => 'text',
-			//	'text' => $text}
+			else{$messages = [
+			'type' => 'text',
+			'text' => $text}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
