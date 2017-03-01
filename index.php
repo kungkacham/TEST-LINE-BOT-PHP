ec45d98dd2789edfb8504311828fdd86993981b2
kungkacham <kungkacham.astrology@gmail.com>
2017-03-02 03:20:30 +0700

    edit

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
+
 $access_token = 'QkERSr95E8xPvTP40mlaIZsY7hBwDKusWmPcp3jChPSxaUF/KNcs8RaVJiThluW6fAHh8jE4vnvsKiHxNnggvcbOtmYk0WHE4FgeNIavK/7F9ypKAHliyjFoK/xlhDHlpy5DJy5rUCKiGgQOVdGbYgdB04t89/1O/w1cDnyilFU=';
+$token = 'QkERSr95E8xPvTP40mlaIZsY7hBwDKusWmPcp3jChPSxaUF/KNcs8RaVJiThluW6fAHh8jE4vnvsKiHxNnggvcbOtmYk0WHE4FgeNIavK/7F9ypKAHliyjFoK/xlhDHlpy5DJy5rUCKiGgQOVdGbYgdB04t89/1O/w1cDnyilFU=';
+$post = file_get_contents(‘php://input’);
+$urlReply = ‘https://api.line.me/v2/bot/message/reply';
+
+function postMessage($token,$packet,$urlReply){
+ $dataEncode = json_encode($packet);
+ $headersOption = array('Content-Type: application/json','Authorization: Bearer '.$token);
+ 
+ $ch = curl_init($urlReply);
+ curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
+ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+ curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
+ curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
+ curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+ 
+}
+
+function getSticker($replyToken){
+ $sticker = array(
+ ‘type’ => ‘sticker’,
+ ‘packageId’ => ‘4’,
+ ‘stickerId’ => ‘300’
+ );
+ $packet = array(
+ ‘replyToken’ => $replyToken,
+ ‘messages’ => array($sticker),
+ );
+ return $packet;
+}
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -174,31 +206,9 @@ if (!is_null($events['events'])) {
 			//	'text' => ว๊ายขี้อวด
 			//];
 			
-			$sticker = [
-			‘type’ => ‘sticker’,
-			‘packageId’ => ‘4’,
-			‘stickerId’ => ‘300’
-			];
+			$packet = getSticker($item[‘replyToken’]);
+			postMessage($token,$packet,$urlReply);
 			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$sticker],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			
-			//echo $result . "\r\n";
 		}
 	}
 }
