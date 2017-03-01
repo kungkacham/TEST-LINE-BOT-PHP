1cbb094b792b2a4dde5ca4b1bcb231e9811b891a
kungkacham <kungkacham.astrology@gmail.com>
2017-03-02 03:47:10 +0700

    edit

--- index.php
+++ index.php
@@ -1,214 +1,75 @@
 <?php
 
-$access_token = 'QkERSr95E8xPvTP40mlaIZsY7hBwDKusWmPcp3jChPSxaUF/KNcs8RaVJiThluW6fAHh8jE4vnvsKiHxNnggvcbOtmYk0WHE4FgeNIavK/7F9ypKAHliyjFoK/xlhDHlpy5DJy5rUCKiGgQOVdGbYgdB04t89/1O/w1cDnyilFU=';
+$post = file_get_contents(‘php://input’);
+$urlReply = ‘https://api.line.me/v2/bot/message/reply';
+$token = 'QkERSr95E8xPvTP40mlaIZsY7hBwDKusWmPcp3jChPSxaUF/KNcs8RaVJiThluW6fAHh8jE4vnvsKiHxNnggvcbOtmYk0WHE4FgeNIavK/7F9ypKAHliyjFoK/xlhDHlpy5DJy5rUCKiGgQOVdGbYgdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+function postMessage($token,$packet,$urlReply){
+ $dataEncode = json_encode($packet);
+ $headersOption = array('Content-Type: application/json','Authorization: Bearer '.$token);
+ 
+ $ch = curl_init($urlReply);
+ curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
+ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+ curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
+ curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
+ curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+ 
+}
 
-			// Build message to reply back
-			//if($text == 'ก้อง' || $text == 'ชาติ'|| $text == 'อั้ม'|| $text == 'ลุง'){
-			//	$messages = [
-			//	'type' => 'text',
-			//	'text' => กาก
-			//];}
-			
-			if($text == 'v' || $text == 'v ' || $text == 'V' || $text == 'V ' || $text == 'view' || $text == 'view ' || $text == 'View' || $text == 'View ' || $text == '.'){
-				$messages = [
-				'type' => 'text',
-				'text' => viewlastseen
-			];}
-			
-			if($text == 's' || $text == 's ' || $text == 'S' || $text == 'S ' || $text == 'set' || $text == 'set ' || $text == 'Set' || $text == 'Set ' || $text == '..'){
-				$messages = [
-				'type' => 'text',
-				'text' => setlastpoint
-			];}		
-			
-			if(strstr($text,'have seen at the lastseen point')){
-				$messages = [
-				'type' => 'text',
-				'text' => setlastpoint
-			];}
-			
-			$array = array('Hi', 'Hello', 'สวัสดีประเทศไทย', 'พักแป๊ป', 'พักแพร๊บ', 'จ๊ะเอ๋', 'สวัสดี', 'สวัสดีครับ', 'สวัสดีค่ะ', 'สวัสดีจ้า', 'สวีดัส สวัสดี', 'สวัสดีจ๊ะ', 'กาก', 'มะกรูด', 'แอแฮร่', 'มาคุยกัน', 'ออกมานะ', 'จับได้แล้ว', 'มีแป้นพิมพ์ไหม', 'คุยได้ไม่กัด แต่จะกัด ถ้าไม่คุย', 'คนพิมพ์น่ารัก', 'คนอ่านน่ารัก', 'ที่นี่ที่ไหน', 'กลุ่มหรือป่าช้า', 'ขอพักบ้างนะ อย่าใช้งานเยอะ', 'สนุกมากไหม มาลองทำเองดูสิ');
-			$output = $array[array_rand($array,1)];
-		
-			if(strstr($text,'Set the lastseens')){
-				$messages = [
-				'type' => 'text',
-				'text' => $output
-			];}
-			
-			if(strstr($text,'บอทกาก')){
-			$messages = [
-				'type' => 'text',
-				'text' => ใครกันแน่ที่กาก
-			];}
-			
-			$arrayyyy = array('เรียนมาน้อยสินะ เลยพิมพ์ได้แค่คำว่า กาก', 'ปมด้อยสินะ ถึงว่าคนอื่นกาก', 'ว่าคนอื่นกาก ก็กากเหมือนกันแหละว้า', 'กาก กาก กาก กาก กาก กาก กาก กาก กาก พอใจม่ะ', 'อย่าหาว่าสอนเลยนะ คำว่า กาก เค้าไว้ใช้เรียกคุณนั่นแหละ', 'ไม่ได้ไปกากบนหัวคุณสักหน่อย', 'พิมพ์คำอื่นไม่เป็นหรือไง', 'กากแล้วไง ไปยุ่งกับเค้าทำไม', 'ใครกันแน่ที่กาก', 'มั่นใจว่ากาก', 'กากแล้วทำไม', 'พ่อแม่ไม่รักสินะ ถึงว่าคนอื่น', 'กาก แล้วไง', 'มีปัญหาป่ะ', 'กาก ก๊าก กาก', 'กากจริง ๆ', 'เดินไปส่องกระจกสิ จะเจอกากกว่า', 'กาก', 'กอ อา กอ กาก', 'ขอบคุณ');
-			$outputttt = $arrayyyy[array_rand($arrayyyy,1)];
-			
-			if(strstr($text,'กาก')){
-			$messages = [
-				'type' => 'text',
-				'text' => $outputttt
-			];}
-			
-			if($text == '...'){
-				$messages = [
-				'type' => 'text',
-				'text' => จุดจุดจุด
-			];}		
-			
-			if(strstr($text,'กลุ่มอะไร')){
-				$messages = [
-				'type' => 'text',
-				'text' => กลุ่มส่งใบโคลเวอร์เกมเศรษฐีครับ
-			];}	
-				
-			if(strstr($text,'ห้องอะไร')){
-				$messages = [
-				'type' => 'text',
-				'text' => ห้องส่งใบโคลเวอร์เกมเศรษฐีครับ
-			];}	
-			
-			$arrayy = array('สวัสดีครับ', 'สวัสดีค่ะ', 'ดีครับ', 'ดีค่ะ', 'ดีคับ', 'หวัดดีคับ', 'หวัดดีครับ', 'หวัดดีค่ะ');
-			$outputt = $arrayy[array_rand($arrayy,1)];
-		
-			if($text == 'สวัสดี' || $text == 'สวัสดีครับ' || $text == 'ดีครับ' || $text == 'ดีคับ' || $text == 'สวัสดีค่ะ' || $text == 'ดีค่ะ' || $text == 'หวัดดีคับ' || $text == 'หวัดดีครับ' || $text == 'หวัดดีค่ะ'){
-				$messages = [
-				'type' => 'text',
-				'text' => $outputt
-			];}		
-			
-			$arrayyy = array('ฝันดีครับ', 'ฝันดีค่ะ', 'นอนหลับฝันดีนะ', 'Sweet dreams.', 'Good Night', 'GN', 'ฝันดีผีกัดตูด', 'ราตรีสวัสดิ์', 'นอนหลับฝันดี ฝันถึงเราด้วยนะ');
-			$outputtt = $arrayyy[array_rand($arrayyy,1)];
-		
-			if($text == 'ราตรีสวัสดิ์' || $text == 'ฝันดี' || $text == 'ง่วงล่ะ' || $text == 'ง่วงแล้ว' || $text == 'นอนกันเถอะ' || $text == 'นอนก่อนนะ' || $text == 'ขอตัวนอนก่อนนะ' || $text == 'ขอตัวนอนก่อน' || $text == 'นอนกันดีกว่า' || $text == 'นอนดีกว่า' || $text == 'พรุ่งนี้ตื่นเช้า' || $text == 'ฝันดีครับ' || $text == 'ฝันดีคับ' || $text == 'ฝันดีค่ะ' || $text == 'นอนหลับฝันดี' || $text == 'นอนแล้วนะ' || $text == 'นอนดีกว่า' || $text == 'นอนก่อนนะ' || $text == 'ขอตัวนอนก่อนนะ' || $text == 'ฝันดีทุกคน' || $text == 'ฝันดี ทุกคน' || $text == 'นอนแล้วครับ' || $text == 'นอนแล้วคับ'){
-				$messages = [
-				'type' => 'text',
-				'text' => $outputtt
-			];}	
-			
-			if(strstr($text,'ฝันดี')){
-				$messages = [
-				'type' => 'text',
-				'text' => ฝันดี
-			];}
-			
-			if(strstr($text,'ดีคับ')){
-				$messages = [
-				'type' => 'text',
-				'text' => สวัสดีครับ
-			];}
-					
-			if(strstr($text,'ดีครับ')){
-				$messages = [
-				'type' => 'text',
-				'text' => สวัสดีครับ
-			];}
-			
-			if(strstr($text,'ดีค่ะ')){
-				$messages = [
-				'type' => 'text',
-				'text' => สวัสดีค่ะ
-			];}
-							
-			//if(strstr($text,'Set the lastseens')){
-			//	$messages = [
-			//	'type' => 'sticker',
-			//	'STKID' => '100',
-			//	'STKPKGID' => '1',
-			//	'STKVER' => '100'
-			//];}
-			
-			//else{$messages = [
-			//'type' => 'text',
-			//'text' => $text}
+function getSticker($replyToken){
+ $sticker = array(
+ ‘type’ => ‘sticker’,
+ ‘packageId’ => ‘4’,
+ ‘stickerId’ => ‘300’
+ );
+ $packet = array(
+ ‘replyToken’ => $replyToken,
+ ‘messages’ => array($sticker),
+ );
+ return $packet;
+}
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-		elseif ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-					
-			//$sticker = array(
-			//'type' => 'sticker',
-			//'STKID' => '100',
-			//'STKPKGID' => '1',
-			//'STKVER' => '100'
-			//);
-			
-			//$sticker = [
-			//	'type' => 'text',
-			//	'text' => ว๊ายขี้อวด
+			//$sticker = 
 			//];
-			
-			$sticker = [
-			‘type’ => ‘sticker’,
-			‘packageId’ => ‘4’,
-			‘stickerId’ => ‘300’
-			];
-			
-			//$sticker = array(
-			//‘type’ => ‘sticker’,
-			//‘packageId’ => ‘4’,
-			//‘stickerId’ => ‘300’
-			//);
-			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$packet = [
-				'replyToken' => $replyToken,
-				'messages' =>[$sticker],
-			];
-			
-			$post = json_encode($packet);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 
-			echo $result . "\r\n";
-		}
-	}
+
+
+function getTextt($replyToken){
+ $text = array(
+ 'type' => 'text',
+ 'text' => ว๊ายขี้อวด
+ );
+ $packet = array(
+ ‘replyToken’ => $replyToken,
+ ‘messages’ => array($text),
+ );
+ return $packet;
+}
+
+ $res = json_decode($post,true);
+if(isset($res[‘events’]) && !is_null($res[‘events’])){
+ foreach($res[‘events’] as $item){
+ if($item[‘type’] == ‘message’){
+ switch($item[‘message’][‘type’]){
+ case ‘text’:	
+ 	$packet = getTextt($item[‘replyToken’]);
+	postMessage($token,$packet,$urlReply);
+ 
+ break;
+ case ‘image’:
+ break;
+ case ‘video’:
+ break;
+ case ‘audio’:
+ break;
+ case ‘location’:
+ break;
+ case ‘sticker’: 
+ break;
 }
 
 echo "OK";
