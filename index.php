75f0b79dea899ecce92d64e1ffa7adbd115466e3
kungkacham <kungkacham.astrology@gmail.com>
2017-03-05 03:20:54 +0700

    edit

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			];}		
 			
 			if(strstr($text,'have seen at the lastseen point')){
-				sleep(1);
+				sleep(3);
 				$messages = [
 				'type' => 'text',
 				'text' => setlastpoint
