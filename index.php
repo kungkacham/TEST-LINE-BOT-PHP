65ba9ee4341b1f4014f8c81f42e595e9729b6bc5
kungkacham <kungkacham.astrology@gmail.com>
2017-04-02 20:58:53 +0700

    edit

--- index.php
+++ index.php
@@ -1,2 +1,45 @@
 <?php
-echo "Hello LINE BOT";
+
+	$servername = "mysql.hostinger.in.th";
+	$database = "u234723275_stic";
+	$username = "u234723275_stic";
+	$password = "5tvsuV2P7drO";
+
+	// Create connection
+
+	$conn = mysqli_connect($servername, $username, $password, $database);
+
+	// Check connection
+
+	if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+	echo "Connected successfully";
+	mysqli_close($conn);
+
+		/*$host = "mysql.hostinger.in.th";
+		$username = "u234723275_stic";
+		$password = "5tvsuV2P7drO";
+		$db="u234723275_stic";
+		$tb="Counter";
+		$conn = mysqli_connect($host,$username,$password,$db);
+		$sql = "INSERT INTO Counter {Counter} VALUES ('2')";
+		mysqli_close($conn);
+		//b4d1e0d7-35a5-43f3-95ca-3ef71cd7187a
+		//kungkacham.astrology@gmail.com
+		mysql_select_db($db);
+		$sql1="Select * From $tb";
+		$dbquery1=mysql_db_query($db,$sql1);
+		$result1= mysql_fetch_array($dbquery1);
+		$Counter=$result1[Counter];
+		$Counter_new=$Counter+1;
+		$sql2="update $tb set Counter='$Counter_new'";
+		$dbquery2=mysql_db_query($db,$sql2);
+		mysql_close();*/
+		
+		
+		
+		
+//echo "Hello LINE BOT";
